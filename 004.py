278b59431baddf9839575905f875192319a822b3
sgilmoreAir11 <stephen@gsector.net>
2016-07-27 01:30:07 -0700

    Completed #4

--- 004.py
+++ 004.py
@@ -11,23 +11,26 @@ max_num = 999 * 999
 def checkit(somenum):
     div = len(str(somenum))//2
     for i in range (0, div):
-        if str(somenum)[i] != str(somenum)[len(str(somenum))-i-1]:
+        if str(somenum) != str(somenum)[::-1]:
             return False
     return True
 
 checknum = max_num
-while checknum > 0:
+while checknum > 100 * 100:
     while checkit(checknum) is False:
         checknum -= 1
-    num_1 = 100
-    while num_1 < checknum / 100:
+        continue
+
+    num_1 = 999
+    while num_1 >= 100:
         num_2 = 999
-        while num_2 >= num_1:
+        while num_2 >= 100:
             if num_1 * num_2 == checknum:
                 print(str(num_1))
-                print(str(num_2))
+                print(str(int(checknum / num_1)))
                 print(str(checknum))
                 exit(0)
-            num_2 += 1
-        num_1 += 1
+            num_2 -= 1
+        num_1 -= 1
+
     checknum -= 1
